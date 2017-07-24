f549c9fbce94946c57e90160fc1dbaa534122682
Jason <lisiyu1990726@gmail.com>
2017-07-24 17:13:28 -0400

    Add file

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,1 @@
-/** * Created by Lee on 7/24/17. */
+/** * Created by Lee on 7/24/17. */let nextTodoId = 0;export const addTodo = text => {    return{        type:'ADD_TODO',        id: nextTodoId++,        text    }};export const setVisibilityFilter = filter => {    return{        type:'SET_VISIBILITY_FILTER',        filter    }};export const toggleTodo = id =>{    return{        type:'TOGGLE_TODO',        id    }};
